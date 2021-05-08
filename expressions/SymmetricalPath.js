b3a92e29757b611e528deb93db90f1d44f618c45
leizingyiu <youcanfindme@126.com>
2021-05-08 08:50:53 +0800

    Update SymmetricalPath.js

--- expressions/SymmetricalPath.js
+++ expressions/SymmetricalPath.js
@@ -1,3 +1,11 @@
+/*
+在路径属性中粘贴此表达式，可生成 根据最后一个锚点对称 的路径
+
+Paste this expression in any path property, to generate Symmetrical path based on the last anchor point .
+
+by Leizingyiu
+
+*/
 var p=points();
 var i=inTangents();
 var o=outTangents();
