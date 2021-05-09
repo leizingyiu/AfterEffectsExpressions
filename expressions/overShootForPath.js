68b81ddd70fff176f544bbda2beddd5237feeea1
leizingyiu <youcanfindme@126.com>
2021-05-10 02:18:01 +0800

    Create overShootForPath.js

---
+++ expressions/overShootForPath.js
@@ -0,0 +1,37 @@
+/* by leizingyiu */
+
+var freq = 3;
+var decay = 5;
+var amp=1;
+
+var n = 0;
+if (thisProperty.numKeys >= 2) {
+	n = thisProperty.numKeys;
+}
+if (thisProperty.numKeys>1 && time>key(n).time) {
+	var t = time - key(n).time;
+	var w = freq * Math.PI * 2;
+	var newT=amp * (Math.sin(t * w) / Math.exp(decay * t) / w);
+	var T=key(n).time-Math.abs(newT);
+
+	var p=points();
+	var i=inTangents();
+	var o=outTangents();
+	var p2=points(T);		
+	var i2=inTangents(T);
+	var o2=outTangents(T);
+
+	if(newT<0){
+		createPath(p2,i2,o2,false);
+	}else{
+		var p3=[];var i3=[];var o3=[];
+		for(let j=0;j<p.length;j++){
+			p3[j]=p[j]*2-p2[j]
+			i3[j]=i[j]*2-i2[j];
+			o3[j]=o[j]*2-o2[j];			
+		}
+		createPath(p3,i3,o3,false);
+		}
+	} else {
+	value
+};
