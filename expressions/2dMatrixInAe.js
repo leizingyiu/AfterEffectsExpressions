4aa8da07e5394bb46075fad56fdf51468dc2f79c
leizingyiu <youcanfindme@126.com>
2021-08-14 01:42:09 +0800

    Update 2dMatrixInAe.js

--- expressions/2dMatrixInAe.js
+++ expressions/2dMatrixInAe.js
@@ -1,49 +1,5 @@
 
-
-function mtrMultMtr0(matrixA, matrixB) {
-
-    let res = [];
-    let b = 0;
-    for (let k = 0; k < 16; k++) {
-        let a = Math.floor(k / 4);
-        let n = 0;
-
-        for (let j = 0; j < 16; j += 4) {
-            console.log([a, b, j]);
-            n += matrixA[a + j] * matrixB[b + j];
-        }
-        b++;
-        if (b == 4) b = 0;
-        res.push(n)
-    }
-    return res;
-}
-
-
-function mtrMultMtr(matrixA, matrixB) {
-
-    let res = [];
-    let b = 0;
-    var l = matrixA.length;
-    console.log(l);
-    for (let k = 0; k < l; k++) {
-        let a = Math.floor(k / Math.pow(l, 0.5));
-        let n = 0;
-
-        for (let j = 0; j < l; j += Math.pow(l, 0.5)) {
-            console.log([a, b, j]);
-            n += matrixA[a + j] * matrixB[b + j];
-        }
-        b++;
-        if (b == Math.pow(l, 0.5)) {
-            b = 0
-        };
-        res.push(n)
-    }
-    return res;
-}
-
-function mtrMultMtr(matrixA, matrixB) {
+function mtrMultMtrConsole(matrixA, matrixB) {
     [...arguments].map(function (arg) {
         if (arg.length == 6) {
             var endM = [0, 0, 1];
@@ -61,13 +17,14 @@ function mtrMultMtr(matrixA, matrixB) {
         let a = Math.floor(k / L);
         let N = 0;
 
+
         for (let j = 0; j < l; j += L) {
             m = j / L;
             n = j;
-            txt += matrixA[a + m] + ' x ' + matrixB[b + n] + ' + ';
-            idTxt += (a + m + 1) + ' x ' + (b + n + 1) + '   ';
+            txt += matrixA[a * L + m] + ' x ' + matrixB[b + n] + ' + ';
+            idTxt += (a * L + m + 1) + ' x ' + (b + n + 1) + '   ';
 
-            N += matrixA[a + m] * matrixB[b + n];
+            N += matrixA[a * L + m] * matrixB[b + n];
         }
         b++;
         if (b == L) {
@@ -84,13 +41,42 @@ function mtrMultMtr(matrixA, matrixB) {
     console.log(idTxt);
     return res;
 }
-M = mtrMultMtr([1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 2, 3, 1, 2, 3, 1, 2, 3])
-console.log(M)
+
+function mtrMultMtr(matrixA, matrixB) {
+    [...arguments].map(function (arg) {
+        if (arg.length == 6) {
+            var endM = [0, 0, 1];
+            endM.map((i, idx) => arg.splice(idx * 3 + 2, 0, i));
+        }
+    })
+    let res = [];
+    let b = 0;
+    let l = matrixA.length;
+    let L = Math.pow(l, 0.5);
+    for (let k = 0; k < l; k++) {
+        let a = Math.floor(k / L);
+        let N = 0;
 
 
+        for (let j = 0; j < l; j += L) {
+            N += matrixA[a * L + j / L] * matrixB[b + j];
+        }
+        b++;
+        if (b == L) {
+            b = 0;
+        }
+        res.push(N)
+    }
+    return res;
+}
 
-mtrMultMtr([1, 0, 1, 1], [1, 0, 4, 5])
-
+function mtrMults() {
+    var arg = [...arguments];
+    arg.splice(0, 2);
+    var result;
+    result = arguments.length > 2 ? mtrMults(mtrMultMtr(arguments[0], arguments[1]), ...arg) : mtrMultMtr(arguments[0], arguments[1]);
+    return result;
+}
 
 function vecMultMtr(v, m) {
     var a = 0, b = 1, c = 3, d = 4, e = 6, f = 7, x = 0, y = 1;
@@ -113,16 +99,16 @@ function scale(x, y) {
 }
 function translate(x, y) {
     return [
-        1, 0, x,
-        0, 1, y,
-        0, 0, 1
+        1, 0, 0,
+        0, 1, 0,
+        x, y, 1
     ];
 }
 
 function rotate(a) {
     return [
-        Math.cos(a), -Math.sin(a), 0,
-        Math.sin(a), Math.cos(a), 0,
+        Math.cos(a), Math.sin(a), 0,
+        -Math.sin(a), Math.cos(a), 0,
         0, 0, 1
     ];
 }
@@ -140,5 +126,28 @@ function skew(a, axisDeg) {
     return mtrMultMtr(mixMatrix, rotate(-axisDeg));
 }
 
-
 translate(-100, -50)
+
+
+
+/** */
+
+
+
+
+
+function vecMultMtr(v, m) {
+    var a = 0, b = 1, c = 3, d = 4, e = 6, f = 7, x = 0, y = 1;
+    if (m.length == 6) {
+        var endM = [0, 0, 1];
+        endM.map((i, idx) => m.splice(idx * 3 + 2, 0, i));
+    }
+    let res = [];
+    res[x] = v[x] * m[a] + v[y] * m[c] + m[e];
+    res[y] = v[x] * m[b] + v[y] * m[d] + m[f];
+    return res;
+}
+console.log(scale(2, 1.5));
+var res = vecMultMtr([80, 80], scale(2, 1.5))
+console.log(res)
+
